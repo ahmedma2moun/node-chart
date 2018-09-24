030094f9246f995e0106838218967609bc291657
gpteamcs <ahmedfcisasu@gmail.com>
2018-09-24 23:12:05 +0200

    add guilocaton

--- public/js/index.js
+++ public/js/index.js
@@ -11,6 +11,14 @@ socket.on('newMessage', function (message) {
     jQuery('#messages').append(li);
 });
 
+socket.on('newLocationMessage', function (message) {
+    var li = jQuery('<li></li>');
+    var a = jQuery(`<a target="_blank">My Carrent location</a>`)
+    li.text(`${message.from}: `);
+    a.attr('href', message.url);
+    li.append(a);
+    jQuery('#messages').append(li);
+});
 
 jQuery('#message-form').on('submit', function (e) {
     e.preventDefault();
@@ -21,4 +29,20 @@ jQuery('#message-form').on('submit', function (e) {
     }, function (data) {
         //  console.log(data);
     });
+});
+
+var locationButton = jQuery('#send-location');
+
+locationButton.on('click', function () {
+    if (!navigator.geolocation) {
+        return alert('No access to location');
+    }
+    navigator.geolocation.getCurrentPosition(function (position) {
+        socket.emit('createLocationMessage', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        });
+    }, function () {
+        alert('Unable to fetch location');
+    })
 });
